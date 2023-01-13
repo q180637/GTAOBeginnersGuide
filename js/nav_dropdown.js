37686f305bd74bac746b6be327a95c91f02208e9
Ben Richardson <q180637@selby.ac.uk>
2023-01-13 11:38:18 +0000

    added top and bottom margins and darker home bg

---
+++ js/nav_dropdown.js
@@ -0,0 +1,16 @@
+function dropdownClick(){
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event){
+    if (!event.target.matches('.dropdown_button')){
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++){
+            var openDropdown = dropdowns[i];
+            if(openDropdown.classList.contains('show')){
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
