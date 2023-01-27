11474c25e935854e014b9246fe830b574b5146a9
Ben Richardson <q180637@selby.ac.uk>
2023-01-27 17:56:32 +0000

    fixed mobile nav and styling i think

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -2,7 +2,8 @@ function burger_flyout()
 {
     var x = document.getElementById("mobile_links");
     if (x.style.display === "none") {
-      x.style.display = "block";
+      x.style.display = "block",
+      x.style.gridColumn = "1 / span 2"
     } else {
       x.style.display = "none";
     }
