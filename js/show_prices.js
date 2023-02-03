281f15f82d623e60dfdb4b6c1deb8436a4cbadf7
BEN RICHARDSON <q180637@selby.ac.uk>
2023-02-03 15:54:18 +0000

    nc and hangar styling adjust

--- js/show_prices.js
+++ js/show_prices.js
@@ -1,8 +1,11 @@
 function hangar_price_toggle() {
-    var x = document.getElementById("trade_price_list");
-    if (x.style.display === "none") {
-      x.style.display = "block";
-    } else {
-      x.style.display = "none";
-    }
+  var x = document.getElementById("trade_price_list");
+  if (x.style.display === "none") {
+    x.style.display = "grid";
+    x.style.gridTemplateColumns = "50% 50%";
+    x.style.gridTemplateRows = "7";
+    x.style.gap ="0.5em";
+  } else {
+    x.style.display = "none";
   }
+}
