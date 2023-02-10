06330cc1957826b91284ab704ddd2d601586ef0e
Ben Richardson <q180637@selby.ac.uk>
2023-02-10 12:08:35 +0000

    fixed mobile nav button

--- js/burger_menu.js
+++ js/burger_menu.js
@@ -1,10 +1,10 @@
 function burger_flyout() 
 {
     var x = document.getElementById("mobile_links");
-    if (x.style.display === "none") {
-      x.style.display = "block",
-      x.style.gridColumn = "1 / span 2"
-    } else {
+    if (x.style.display === "block") {
       x.style.display = "none";
+    } 
+    else {
+      x.style.display = "block";
     }
 }
