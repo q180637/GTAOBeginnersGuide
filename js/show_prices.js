4906d63c33f046ce437ea3ea09397b737cd32705
Ben Richardson <q180637@selby.ac.uk>
2023-02-10 13:28:02 +0000

    trying to sort out the hangar button

--- js/show_prices.js
+++ js/show_prices.js
@@ -1,11 +1,11 @@
 function hangar_price_toggle() {
   var x = document.getElementById("trade_price_list");
-  if (x.style.display === "none") {
-    x.style.display = "grid";
+  if (x.style.display === "grid") {
+    x.style.display = "none";
     x.style.gridTemplateColumns = "50% 50%";
     x.style.gridTemplateRows = "7";
     x.style.gap ="0.5em";
   } else {
-    x.style.display = "none";
+    x.style.display = "grid";
   }
 }
