d28070a9ded01395bc3af42c3cad6b9357ac07ca
BEN RICHARDSON <q180637@selby.ac.uk>
2023-02-10 14:18:20 +0000

    got button working properly

--- js/show_prices.js
+++ js/show_prices.js
@@ -2,10 +2,12 @@ function hangar_price_toggle() {
   var x = document.getElementById("trade_price_list");
   if (x.style.display === "grid") {
     x.style.display = "none";
-    x.style.gridTemplateColumns = "50% 50%";
+    x.style.gridTemplateColumns = "100%";
     x.style.gridTemplateRows = "7";
     x.style.gap ="0.5em";
-  } else {
+    x.style.paddingLeft = "0";
+  } 
+  else {
     x.style.display = "grid";
   }
 }
